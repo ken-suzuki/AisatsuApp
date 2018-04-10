aa4247f24be4e1e8d30bdd699d94924ba79333ae
ken-suzuki <yunalovely1977@yahoo.co.jp>
2018-04-10 17:16:22 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/ken/suzuki/aisatsuapp/MainActivity.java
@@ -0,0 +1,55 @@
+package jp.techacademy.ken.suzuki.aisatsuapp;
+
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    public void onClick(View v) {
+            showTimePickerDialog();
+        textView.setText(textView.getText().toString());
+    }
+
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        if (hourOfDay < 10) {
+                            textView.setText("おはよう");
+                        } else if (hourOfDay < 18) {
+                            textView.setText("こんにちは");
+                        } else {
+                            textView.setText("こんばんは");
+                        }
+                    }
+                },
+                13, // 初期値（時間）
+                0,  // 初期値（分）
+                true);
+        timePickerDialog.show();
+    }
+}
